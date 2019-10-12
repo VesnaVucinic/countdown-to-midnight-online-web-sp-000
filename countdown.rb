4a1757b9e019881cb42c13400bb8b5575a0e6193
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-12 16:31:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,7 +11,7 @@ countdown (num_of_sec)
 
 num_of_sec = 10 
 
-def countdown (num_of_sec)
+def countdown_with_sleep (num_of_sec)
   while num_of_sec > 0 
     puts "#{num_of_sec} SECOND(S)!"
     num_of_sec -= 1 
