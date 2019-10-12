6fdfe5d792eac72f9bbcb8bf531cf9e9796abe30
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-12 16:21:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@ num_of_sec = 10
 def countdown (num_of_sec)
   while num_of_sec > 0 
     puts "#{num_of_sec} SECOND(S)!"
-    slip 1.second 
+    sleep 1
     num_of_sec -= 1 
   end
   #return "HAPPY NEW YEAR!"
